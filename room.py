af6d194236315188aa562a2182e8d5a8df9fd134
William Joshua King <wgk4@duke.edu>
2022-11-12 03:02:38 -0500

    Added Visualization

--- room.py
+++ room.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
@@ -33,6 +34,17 @@ class Room:
             self.angles.append(math.atan2((corners[0][1] - corners[len(corners)-1][1]),
                                            (corners[0][0] - corners[len(corners)-1][0])))
 
+    def plot_room(self):
+        plt.figure()
+        for i in range(len(self.corners)-1):
+            xs = [self.corners[i][0], self.corners[i+1][0]]
+            ys = [self.corners[i][1], self.corners[i+1][1]]
+            plt.plot(xs, ys, 'b')
+        plt.plot([self.corners[len(self.corners)-1][0], self.corners[0][0]],
+                 [self.corners[len(self.corners)-1][1], self.corners[0][1]], 'b')
+        plt.show()
+
 
 if __name__ == "__main__":
-    room = Room(corners=[[0,0],[-1,15],[5,12],[5,1]])
+    room = Room(corners=[[0, 0], [-1, 15], [5, 12], [5, 1]])
+    room.plot_room()
